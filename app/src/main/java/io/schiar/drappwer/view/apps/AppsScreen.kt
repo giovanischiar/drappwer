5b3cee32662749d73bc7e00e6e6bf075376bd18a
Giovani Schiar <giovanischiar@gmail.com>
2024-06-17 13:30:57 -0700

    Create a New Layout for Apps Screen

--- app/src/main/java/io/schiar/drappwer/view/apps/AppsScreen.kt
+++ app/src/main/java/io/schiar/drappwer/view/apps/AppsScreen.kt
@@ -3,11 +3,14 @@ package io.schiar.drappwer.view.apps
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
@@ -17,17 +20,32 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.foundation.ExperimentalWearFoundationApi
+import androidx.wear.compose.foundation.lazy.AutoCenteringParams
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumnDefaults
+import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
+import androidx.wear.compose.foundation.rememberActiveFocusRequester
+import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.ChipColors
+import androidx.wear.compose.material.ChipDefaults
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
@@ -35,8 +53,9 @@ import io.schiar.drappwer.view.shared.theme.DrappwerTheme
 import io.schiar.drappwer.view.shared.toBitmap
 import io.schiar.drappwer.view.shared.toByteArray
 import io.schiar.drappwer.viewmodel.AppViewModel
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalLayoutApi::class)
+@OptIn(ExperimentalLayoutApi::class, ExperimentalWearFoundationApi::class)
 @Composable
 fun AppsScreen(viewModel: AppViewModel) {
     val context = LocalContext.current
@@ -73,6 +92,10 @@ fun AppsScreen(viewModel: AppViewModel) {
         )
     }
 
+    val listState = rememberScalingLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val focusRequester = rememberActiveFocusRequester()
+
     DrappwerTheme {
         Box(
             modifier = Modifier
@@ -81,33 +104,64 @@ fun AppsScreen(viewModel: AppViewModel) {
             contentAlignment = Alignment.Center
         ) {
             TimeText()
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .padding(all = 30.dp)) {
-                FlowRow {
-                    selectedApps.map { app ->
-                        val (_, icon) = app
-                        Image(
-                            modifier = Modifier.size(20.dp),
-                            bitmap = icon.toBitmap().asImageBitmap(),
-                            contentDescription = ""
+            Box(modifier = Modifier.fillMaxSize()) {
+                ScalingLazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .onRotaryScrollEvent {
+                            println("rotary scroll event: $it")
+                            coroutineScope.launch { listState.scrollBy(it.verticalScrollPixels) }
+                            true
+                        }
+                        .focusRequester(focusRequester)
+                        .focusable(),
+                    autoCentering = AutoCenteringParams(itemIndex = 0),
+                    state = listState,
+                    flingBehavior = ScalingLazyColumnDefaults.snapFlingBehavior(
+                        state = listState,
+                        snapOffset = 0.dp
+                    )
+                ) {
+                    items(count = apps.size) { index ->
+                        val (appName, icon, selected) = apps[index]
+                        Chip(
+                            modifier = Modifier.fillMaxWidth(),
+                            label = {
+                                Text(
+                                    text = appName,
+                                    color = if (selected) Color.Black else Color.White
+                                )
+                            },
+                            onClick = {
+                                viewModel.selectAppOf(index = index)
+                            },
+                            icon = {
+                                Image(
+                                    modifier = Modifier.size(20.dp),
+                                    bitmap = icon.toBitmap().asImageBitmap(),
+                                    contentDescription = ""
+                                )
+                            },
+                            colors = ChipDefaults.chipColors(
+                                backgroundColor = if (selected) Color.Yellow else MaterialTheme.colors.surface
+                            )
                         )
                     }
                 }
 
-                LazyColumn {
-                    items(count = apps.size) { index ->
-                        val (appName, icon, selected) = apps[index]
-                        Row(modifier = Modifier.fillMaxWidth().background(color = if (selected) Color.DarkGray else Color.Transparent).clickable {
-                            viewModel.selectAppOf(index = index)
-                        }) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp),
+                    contentAlignment = Alignment.TopCenter) {
+                    FlowRow {
+                        selectedApps.map { app ->
+                            val (_, icon) = app
                             Image(
                                 modifier = Modifier.size(20.dp),
                                 bitmap = icon.toBitmap().asImageBitmap(),
                                 contentDescription = ""
                             )
-                            Spacer(modifier = Modifier.width(5.dp))
-                            Text(text = appName)
                         }
                     }
                 }
